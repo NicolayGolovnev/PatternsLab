968096258a75827e5a779b80bb75d6e79a4d62c3
Nicolay Golovnev <kolya.golovnev@mail.ru>
2021-10-25 23:22:36 +0700

    Made proxy-filter for lazy initialization pattern

--- Proxy/ProxyFilter.h
+++ Proxy/ProxyFilter.h
@@ -10,21 +10,24 @@
 
 class ProxyFilter : public Filter {
 private:
-    RealFilter* filter;
+    RealFilter* filter = NULL;
     void log(){
         printf("\n[ProxyLog]\tResult: ");
     }
 
 public:
-    ProxyFilter(){
-        filter = new RealFilter();
-    }
+    ProxyFilter() {}
+    ~ProxyFilter() {}
     void initFilter(RealFilter* inpFilter){
         if (filter != NULL)
             delete filter;
         filter = inpFilter;
     }
-    virtual int getResult() override {
+    int getResult() override {
+        if (filter == NULL) {
+            printf("Proxy filter didn't initialize!");
+            return -1;
+        }
         log();
         return filter->getResult();
     }
